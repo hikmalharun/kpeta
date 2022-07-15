1efc66349127341591927ad97f03fcaf3e630c92
hikmalharun <hikmal.harun@gmail.com>
2022-07-15 23:42:22 +0700

    Rekap Absen

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -232,7 +232,7 @@
                     <div class="container-fluid">
                         <h3 class="m-2"><strong>DATA ABSEN PULANG BULAN <?php echo $month; ?></strong></h3>
                         <div class="table-responsive m-2">
-                            <table class="table table-bordered">
+                            <table class="table table-bordered" id="table">
                                 <thead>
                                     <tr>
                                         <th>Tanggal</th>
@@ -266,7 +266,7 @@
                         <div class="container-fluid">
                             <h3 class="m-2"><strong>DATA ABSEN MASUK BULAN <?php echo $month; ?></strong></h3>
                             <div class="table-responsive m-2">
-                                <table class="table table-bordered">
+                                <table class="table table-bordered" id="table">
                                     <thead>
                                         <tr>
                                             <th>Nama</th>
